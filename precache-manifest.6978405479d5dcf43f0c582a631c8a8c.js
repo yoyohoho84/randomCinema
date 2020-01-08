938657066900d161a5245b324e80b6575f4d76a2
Kirill <yoyohoho84@gmail.com>
2020-01-08 18:08:19 +0300

    Updates

---
+++ precache-manifest.6978405479d5dcf43f0c582a631c8a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b27938d412195c98c11a220aa057b5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dd64e33c8b05435a0c29",
+    "url": "./static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "c0d0f8bb22c4009fff0e",
+    "url": "./static/css/main.25beb645.chunk.css"
+  },
+  {
+    "revision": "dd64e33c8b05435a0c29",
+    "url": "./static/js/2.80cb265a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.80cb265a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0d0f8bb22c4009fff0e",
+    "url": "./static/js/main.fde6ce02.chunk.js"
+  },
+  {
+    "revision": "622d6a3f6e53278a7581",
+    "url": "./static/js/runtime-main.72b5e4d2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9071fd68c75b5a6eff3ba3b9d8942577.js"
+  "./precache-manifest.6978405479d5dcf43f0c582a631c8a8c.js"
 );
 
 self.addEventListener('message', (event) => {
