ee1fe631a312b77963638c7730a91c1b88fd00f0
Kirill <yoyohoho84@gmail.com>
2020-03-14 11:01:56 +0300

    Updates

---
+++ precache-manifest.92481cb0619b4a33546f5a5f491c4f37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ab7bfeb9ecd54f7832478c098f2f46",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b2bf8f24f9818d7d777",
+    "url": "./static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "f012cddc1580881b626b",
+    "url": "./static/css/main.7804f2e3.chunk.css"
+  },
+  {
+    "revision": "3b2bf8f24f9818d7d777",
+    "url": "./static/js/2.b7bcb81e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.b7bcb81e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f012cddc1580881b626b",
+    "url": "./static/js/main.2d36751c.chunk.js"
+  },
+  {
+    "revision": "622d6a3f6e53278a7581",
+    "url": "./static/js/runtime-main.72b5e4d2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4f317098cb1b8709c60db6ccd01a4c40.js"
+  "./precache-manifest.92481cb0619b4a33546f5a5f491c4f37.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
